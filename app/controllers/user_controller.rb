fc2914e705861cf51d0860fc76c1c11dc612b7b6
Pedro-Pezoa <pedroluizpezoa@gmail.com>
2018-02-23 17:44:11 -0300

    Implementation of the devise gem
    The devise gem now is full operational, with changes that include: Added a controller, model and table for the User; All of the html.slim files(except the forgot password) were changed to use the devise gem methods; Changes in core files to utilize the gem, like root.rb, application.rb and development.rb; Added a custom class for login failure and a devise initializer file.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,9 @@
 class UserController < ApplicationController
 
 	def home
-		
+		if user_signed_in?
+      redirect_to graphic_path
+    end
 	end
 
 	def forgot_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root to: "user#index"
+  root to: "user#home"
   get "/forgot_password", to: "user#forgot_password", as: "forgot_password"
-  get "/spreadsheet", to: "home#spreadsheet", as: "spreadsheet"
   get "/graphic", to: "home#graphic", as: "graphic"
+  get "/spreadsheet", to: "home#spreadsheet", as: "spreadsheet"
  
 end
