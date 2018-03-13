fad1128a52c1dcab1efc45acb78eccc1c42cd866
Pedro-Pezoa <pedroluizpezoa@gmail.com>
2018-03-13 20:13:08 -0300

    Implementing contract data view in graphics
    The graphic view feature have been update to also show the contract data linked with a report and a user(which is the salesman). However, the method has some bugs of skipping data of other days.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,11 +18,17 @@ class HomeController < ApplicationController
   end
 
 	def graphic
-		
+		if $current_report == nil
+			$current_report = Report.first
+			calculate_data
+		end
 	end
 
 	def spreadsheet
-		
+		if $current_report == nil
+			$current_report = Report.first
+			calculate_data
+		end
 	end
 
 	def searchS
@@ -83,6 +89,23 @@ class HomeController < ApplicationController
 		end
 	end
 
+	def search_contracts(_contract_list, _index, _current_day)
+		partial_sum = 0
+		partial_vendor_names = ""
+		partial_store_names = ""
+		value = 1
+		while _contract_list[_index] != nil && _contract_list[_index].day == _current_day
+      partial_sum += _contract_list[_index].value
+			partial_vendor_names += value.to_s + "-" + User.find_by_id(_contract_list[_index].user_id).full_name + ", "
+			partial_store_names += value.to_s + "-" + _contract_list[_index].store_name + ", "
+			_index += 1
+			value += 1
+		end
+
+		list = [_index, partial_sum, partial_store_names, partial_vendor_names]
+		list
+	end
+
 	def calculate_report_data
 		month_days = Time.days_in_month $current_report.month_numb
 		weekdays = find_weekdays month_days
@@ -94,8 +117,13 @@ class HomeController < ApplicationController
 		value = const
 		factor = 2
 
+		contract_sum = 0
+		contracts_data_index = 0
+		contracts_data = Contract.where(:report_id => $current_report.id).order('day')
+
     for day in 1..month_days
-    	day_data = [] # day_data[0] = day - day_data[1] = weekday - day_data[2] = daily goal
+    	day_data = [] # day_data[0] = day - day_data[1] = weekday - day_data[2] = daily goal - day_data[3] = contract sum
+    	              # day_data[4] = store name - day_data[5] = contract value - day_data[6] = salesman name
     	
     	date = Date.strptime($current_report.month_numb.to_s + "/" + day.to_s + "/" + $current_report.year.to_s, '%m/%d/%Y')
       day_data << day.to_s
@@ -105,8 +133,38 @@ class HomeController < ApplicationController
 	      value = const * factor
 	    	factor += 1
 	    end
-
 	    day_data << value.to_s
+
+			if not contracts_data.empty?
+		    if contracts_data[contracts_data_index].day == day
+		    	if contracts_data[contracts_data_index+1].day == day
+		    		partial_list = search_contracts contracts_data, contracts_data_index, day
+
+		    		contracts_data_index = partial_list[0]
+		    		contract_sum += partial_list[1]
+
+		    		day_data[3] = contract_sum
+		    		day_data[4] = partial_list[2]
+		    		day_data[5] = partial_list[1]
+		    		day_data[6] = partial_list[3]
+
+		    	else
+		    		contracts_data_index += 1
+		    		contract_sum += contracts_data[contracts_data_index].value
+
+		    		day_data[3] = contract_sum
+		    		day_data[4] = contracts_data[contracts_data_index].store_name
+		    		day_data[5] = contracts_data[contracts_data_index].value
+		    		day_data[6] = User.find_by_id(contracts_data[contracts_data_index].user_id).full_name
+		    	end
+				end
+	    else
+		    day_data[3] = 0
+	  		day_data[4] = "-"
+	  		day_data[5] = 0
+	  		day_data[6] = "-"
+	  	end
+
       $current_report_data << day_data
     end
 	end
