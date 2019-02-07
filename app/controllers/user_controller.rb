5d4463309e591f9af47de1abdd414c13406a5f2e
Pedro-Pezoa <pedroluizpezoa@gmail.com>
2019-02-07 14:50:46 -0200

    Removed the sign up feature
    The sign up feature have been removed as the registration process became very dangerous for the application's data.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,27 +3,26 @@ class UserController < ApplicationController
   before_action :generate_token, only: [:after_forgot_password]
   layout 'main'
 
-  def user_home
-    report = fetch_last_report
-    @current_month = report.month
-    @current_year = report.year
-  end
-
-  def edit_password
-    redirect_to root_path unless verify_token_usage(params[:token])
-  end
+  def user_home; end
 
   def forgot_password; end
 
-  def register; end
+  def edit_password
+    redirect_to root_path unless fetch_token_usage(params[:token])
+  end
+
+  def root_without_locale
+    redirect_to root_path(locale: :pt)
+  end
 
   def after_forgot_password
     zapper = ZapierRuby::Zapper.new(:email_zap)
-    result = if zapper.zap(json_maker(@user_email,
-                                      'Edit password link', @link))
-               'Email enviado com sucesso'
+    result = if zapper.zap(
+      json_maker(@user_email, t('home.zapier-content'), @link)
+    )
+               t('home.zapier-success')
              else
-               'Falha ao enviar email'
+               t('home.zapier-invalid')
              end
     redirect_to root_path, notice: result
   end
@@ -33,6 +32,7 @@ class UserController < ApplicationController
   def generate_token
     @token = SecureRandom.base58(24)
     @user_email = params[:user][:email]
+
     save_token(fetch_user_by_email(@user_email).id)
     @link = ENV['link_to_root'] + edit_password_path +
             '?token=' + @token + '&email=' + @user_email
@@ -43,7 +43,7 @@ class UserController < ApplicationController
   end
 
   def verify_user_status
-    redirect_to monthly_sales_path if user_signed_in?
+    redirect_to overview_months_AM_path(locale: :pt) if user_signed_in?
     @user = User.new
   end
 end
