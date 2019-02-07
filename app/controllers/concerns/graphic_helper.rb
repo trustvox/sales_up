2e2dd6b8c78f2b963da1905eb64ba61ee0ecb6cc
Pedro-Pezoa <pedroluizpezoa@gmail.com>
2019-02-07 14:59:22 -0200

    Database centralized, contract model and helper updated, new meeting model and register page removed
    The files listed have been changed to: 1-Centralize the database searchs in different files and connect them with a main database file; 2-Update the contract model for new verifications and also update the graphic helper; 3-Create a new meeting model not added in the previous commit; 4-Remove the registration page for good inside the application.

--- app/controllers/concerns/graphic_helper.rb
+++ app/controllers/concerns/graphic_helper.rb
@@ -3,23 +3,91 @@ module GraphicHelper
     (1..month_days).collect { |day| [day, day.to_s] }
   end
 
-  def month_text_generator
+  def wday_text_generator
+    date = Date.new(@current_report.year, @current_report.month_numb, 1) - 1
+    (1..month_days).collect do |day|
+      date += 1
+      [day, t('menu.spreadsheet.' + Date::DAYNAMES[date.wday])[0]]
+    end
+  end
+
+  def month_text_generator(type)
     @month_text = []
     report = @first_report
     i = 1
     while proceed?(report)
       add_month_text(i, report)
-      report = fetch_report_by_next_month(report)
+      report = fetch_report_by_next_month(report, type)
       i += 1
     end
     add_month_text(i, report)
   end
 
   def add_month_text(index, report)
-    @month_text << [index, report.month[0..2] + '/' + report.year.to_s[2..3]]
+    @month_text << [index, t('menu.search.' + report.month)[0..2] + '/' +
+                           report.year.to_s[2..3]]
   end
 
   def proceed?(report)
     !report.nil? && report.id != @last_report.id
+  end
+
+  def init_overview_options_list(type)
+    list = if type == 'AM'
+             [[t('overview.AM.cs'), 'CS'],
+              [t('overview.AM.cc'), 'CC'],
+              [t('overview.AM.cp'), 'CP']]
+           else
+             [[t('overview.SDR.ms'), 'MS'],
+              [t('overview.SDR.mp'), 'MP']]
+           end
+    create_options_list(type, list)
+  end
+
+  def create_options_list(type, list)
+    @user_options =
+      [[t('overview.' + type + '.all'), 'All']] +
+      fetch_username_by_sub_area(type).collect { |user| [user, user] }
+    @filter_options = list
+  end
+
+  def verify_filter
+    @filter_options.each do |op|
+      next unless op[1] == params[:report][:goal]
+      @filter_options.delete(op)
+      @filter_options.unshift(op)
+    end
+  end
+
+  def verify_user
+    @user_options.each do |op|
+      next unless op[1] == params[:report][:report_name]
+      @user_options.delete(op)
+      @user_options.unshift(op)
+    end
+  end
+
+  def verify_options
+    verify_filter
+
+    return if params[:report][:report_name] == 'All'
+    verify_user
+  end
+
+  def find_weekdays
+    (@first..@last).collect { |date| verify_weekday(date) }
+  end
+
+  def verify_weekday(date)
+    return 'Monday' if date.monday?
+    return 'Tuesday' if date.tuesday?
+    return 'Wednesday' if date.wednesday?
+    return 'Thursday' if date.thursday?
+    return 'Friday' if date.friday?
+    verify_weekend(date)
+  end
+
+  def verify_weekend(date)
+    date.saturday? ? 'Saturday' : 'Sunday'
   end
 end
