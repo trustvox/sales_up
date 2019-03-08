fa16d8591193bfd239177d4ee780965ff57264e4
Pedro-Pezoa <pedroluizpezoa@gmail.com>
2019-03-08 13:12:31 -0300

    Update spec tests with the new area/sub_area attribute
    For Travis build changes, the test regarding user model were updated to use the new area/sub_area attributes instead of the priority type.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:priority) { 0 }
-  let!(:user) { build(:user, priority: priority) }
+  let(:area) { 'spec' }
+  let(:sub_area) { 'spec' }
+  let!(:user) { build(:user, area: area, sub_area: sub_area) }
 
   context 'instance methods' do
-    describe '#regular?' do
-      let(:priority) { 1 }
+    describe '#overall_manager?' do
+      let(:area) { 'gg' }
 
-      it 'responds to :regular?' do
-        expect(user).to respond_to(:regular?)
+      it 'responds to :overall_manager?' do
+        expect(user).to respond_to(:overall_manager?)
       end
 
-      it 'checks given user is a regular one' do
-        expect(user).to be_regular
+      it 'checks given user is a overall manager one' do
+        expect(user).to be_overall_manager
+      end
+    end
+
+    describe '#sales?' do
+      let(:area) { 'sales' }
+
+      it 'responds to :sales?' do
+        expect(user).to respond_to(:sales?)
+      end
+
+      it 'checks given user is from sales one' do
+        expect(user).to be_sales
       end
     end
 
     describe '#admin?' do
-      let(:priority) { 3 }
+      let(:sub_area) { 'admin' }
 
       it 'responds to :admin?' do
         expect(user).to respond_to(:admin?)
